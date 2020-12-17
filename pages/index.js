1608ffd63f6fd2255d3c0aa498cbc54a457765b0
mohamed <mohamedsahel55@gmail.com>
2020-12-17 22:00:37 +0100

    add authentication with Github

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
+
 import styles from '../styles/Home.module.css'
+import { useAuth } from '../lib/auth'
+
 
 export default function Home() {
+  const auth = useAuth()
+
+  useEffect(() => {
+    console.log(auth)
+  }, [auth])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,48 +28,14 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {
+          !auth.user && <button onClick={() => auth?.signinWithGithub()} >Signin With Github</button>
+        }
+        <div> {auth.user ? auth.user.email : 'Not Authenticated'} </div>
+        {
+          auth.user && <button onClick={() => auth.signout()} >signout</button>
+        }
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
